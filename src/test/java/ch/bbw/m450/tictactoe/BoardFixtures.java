836f3d1c0354801a49c652a280e0757660101aed
Schibli Emil <Emil.Schibli@swisscom.com>
2026-09-08 22:14:45 +0200

    add parameterized isWin tests

--- src/test/java/ch/bbw/m450/tictactoe/BoardFixtures.java
+++ src/test/java/ch/bbw/m450/tictactoe/BoardFixtures.java
@@ -2,12 +2,9 @@ package ch.bbw.m450.tictactoe;
 
 class BoardFixtures {
 
-	static final String TOP_ROW_CROSS_WIN = "XXX......";
-	static final String MIDDLE_COLUMN_CROSS_WIN = ".X..X..X.";
-	static final String MAIN_DIAGONAL_CROSS_WIN = "X...X...X";
-	static final String NO_WINNER = "XOX.OX...";
-	static final String TOP_ROW_CIRCLE_WIN = "OOO......";
-
-	private BoardFixtures() {
-	}
+	static final String TOP_ROW_X = "XXX......";
+	static final String MID_COL_X = ".X..X..X.";
+	static final String DIAG_X = "X...X...X";
+	static final String NO_WIN = "XOX.OX...";
+	static final String TOP_ROW_O = "OOO......";
 }
