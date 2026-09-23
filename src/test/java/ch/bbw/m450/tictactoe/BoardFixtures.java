56550d65294c721ecdb91aec6ec2312d5dfb3910
Schibli Emil <Emil.Schibli@swisscom.com>
2026-09-23 11:03:51 +0200

    add tests for play, players and stone

--- src/test/java/ch/bbw/m450/tictactoe/BoardFixtures.java
+++ src/test/java/ch/bbw/m450/tictactoe/BoardFixtures.java
@@ -2,9 +2,16 @@ package ch.bbw.m450.tictactoe;
 
 class BoardFixtures {
 
+	static final String EMPTY = ".........";
 	static final String TOP_ROW_X = "XXX......";
+	static final String MID_ROW_O = "...OOO...";
+	static final String BOTTOM_ROW_X = "......XXX";
+	static final String LEFT_COL_O = "O..O..O..";
 	static final String MID_COL_X = ".X..X..X.";
+	static final String RIGHT_COL_O = "..O..O..O";
 	static final String DIAG_X = "X...X...X";
+	static final String ANTI_DIAG_O = "..O.O.O..";
 	static final String NO_WIN = "XOX.OX...";
 	static final String TOP_ROW_O = "OOO......";
+	static final String FULL_DRAW = "XOXXOOOXX";
 }
